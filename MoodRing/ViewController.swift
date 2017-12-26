1247f491ed3d5dd044e0f510b7626f2b467c6c47
flizzer <briandavis1977@gmail.com>
2017-12-25 22:45:56 -0500

    Initial Commit

---
+++ MoodRing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoodRing
+//
+//  Created by Brian Davis on 12/25/17.
+//  Copyright © 2017 Brian Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
