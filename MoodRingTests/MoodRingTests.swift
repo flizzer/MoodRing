adbc5cd6aaf2bd2e399709d0c9c52067d24d75d2
flizzer <briandavis1977@gmail.com>
2017-12-27 22:56:24 -0500

    getting testViewControllerSetup unit test passing

--- MoodRingTests/MoodRingTests.swift
+++ MoodRingTests/MoodRingTests.swift
@@ -11,26 +11,24 @@ import XCTest
 
 class MoodRingTests: XCTestCase {
     
+    var viewController : ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyBoard = UIStoryboard(name: "Main"
+            , bundle: Bundle.main)
+        viewController = storyBoard
+            .instantiateViewController(withIdentifier: "ViewController")
+            as! ViewController
+        let _ = viewController.view
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testViewControllerSetup() {
+        XCTAssertTrue(true)
     }
     
 }
